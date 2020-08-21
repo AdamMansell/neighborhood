091bc4a66b2767a5d260295bf5fb018d4e1ddce1
AdamManselll <adammansellhmb@gmail.com>
2020-08-21 09:28:44 -0700

    Take given string, make it integer, and print it.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,22 @@
 $(document).ready(function () {
   $("#formOne").submit(function (event) {
-    function vowBegin(sentence) {
-      if (/[aeiou]/i.test(sentence)) {
-        sentence += "way";
-        return sentence;
-      } else {
-        return sentence;
-      }
-    }
-    console.log(vowBegin("octopus"));
     event.preventDefault();
+    let givenString = $("input#range").val();
+    let givenNum = parseInt(givenString);
+    let arrayRange = []
+    arrayRange.push(givenNum);
+    $(".resulth3").text(arrayRange);
+    $("#result").show();
+
+    // function toArray(sentence) {
+    //   if (/[1]/i.test(sentence)) {
+    //     sentence += "way";
+    //     return sentence;
+    //   } else {
+    //     return sentence;
+    //   }
+    // }
+    // console.log(vowBegin("octopus"));
+    // event.preventDefault();
   });
 });
