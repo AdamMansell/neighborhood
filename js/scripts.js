40d91ceafaf591a97fa1fc4c8180317c9ca20da5
AdamManselll <adammansellhmb@gmail.com>
2020-09-01 20:47:35 -0700

    Make Final Update

--- js/scripts.js
+++ js/scripts.js
@@ -5,32 +5,23 @@ function Logic(givenString, goodInput, badInput, resultH3) {
   if ((/^\d+$/.test(givenNum)) === true) {
     for (i = 0; i <= givenNum; i++) {
       arrayRange.push(i);
-      // $(".resulth3").text(arrayRange);
     }
     for (i = 0; i <= arrayRange.length; i++) {
       console.log(arrayRange);
       if ((/1/.test(arrayRange[i]) == true) && (/2/.test(arrayRange[i]) == false) && (/3/.test(arrayRange[i]) == false)) {
         arrayRange.splice(arrayRange[i], 1, "Beep!");
-        // resultH3.text(arrayRange);
-        // return arrayRange;
       } else if ((/2/.test(arrayRange[i]) == true) && (/3/.test(arrayRange[i]) == false)) {
         arrayRange.splice(arrayRange[i], 1, "Boop!");
-        // resultH3.text(arrayRange);
-        // return arrayRange;
       } else if (/3/.test(arrayRange[i]) == true) {
         arrayRange.splice(arrayRange[i], 1, "Won't you be my neighbor?");
-        // resultH3.text(arrayRange);
-        // return arrayRange;
       }
     }
     badInput.hide() && goodInput.show();
   } else {
     goodInput.hide() && badInput.show();
   }
+  return arrayRange;
 }
-
-
-
 
 $(document).ready(function () {
   $("#formOne").submit(function (event) {
@@ -41,31 +32,5 @@ $(document).ready(function () {
     let badInput = $("#badResult");
     let newList = new Logic(givenString, goodInput, badInput, resultH3);
     resultH3.text(newList);
-
-
-
-    // if ((/^\d+$/.test(givenString)) === true) {
-    //   for (i = 0; i <= givenNum; i++) {
-    //     arrayRange.push(i);
-    //     $(".resulth3").text(arrayRange);
-    //   }
-    //   for (i = 0; i <= arrayRange.length; i++) {
-    //     if ((/1/.test(arrayRange[i]) == true) && (/2/.test(arrayRange[i]) == false) && (/3/.test(arrayRange[i]) == false)) {
-    //       arrayRange.splice(arrayRange[i], 1, "Beep!");
-    //       $(".resulth3").text(arrayRange);
-    //     } else if ((/2/.test(arrayRange[i]) == true) && (/3/.test(arrayRange[i]) == false)) {
-    //       arrayRange.splice(arrayRange[i], 1, "Boop!");
-    //       $(".resulth3").text(arrayRange);
-    //     } else if (/3/.test(arrayRange[i]) == true) {
-    //       arrayRange.splice(arrayRange[i], 1, "Won't you be my neighbor?");
-    //       $(".resulth3").text(arrayRange);
-    //     }
-    //   }
-    //   $("#badResult").hide();
-    //   $("#result").show();
-    // } else {
-    // $("#result").hide();
-    // $("#badResult").show();
-    // }
   });
 });
